c54652da99bcdbd19843fcc27b609d8a221be8da
Justin Wong <stryderjzw@gmail.com>
2024-01-05 14:38:13 -0800

    redirect example

--- lib/islands_interface_web/controllers/page_controller.ex
+++ lib/islands_interface_web/controllers/page_controller.ex
@@ -4,6 +4,10 @@ defmodule IslandsInterfaceWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    redirect(conn, to: ~p"/redirect-test")
+  end
+
+  def redirect_test(conn, _params) do
     render(conn, :home, layout: false)
   end
 end
