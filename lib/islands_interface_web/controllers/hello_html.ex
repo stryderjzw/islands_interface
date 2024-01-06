0870be2b59c87e0c5314426f6b33992c38cdb0d9
Justin Wong <stryderjzw@gmail.com>
2024-01-05 21:29:49 -0800

    create function component

--- lib/islands_interface_web/controllers/hello_html.ex
+++ lib/islands_interface_web/controllers/hello_html.ex
@@ -2,4 +2,12 @@ defmodule IslandsInterfaceWeb.HelloHTML do
   use IslandsInterfaceWeb, :html
 
   embed_templates "hello_html/*"
+
+  attr :messenger, :string
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Hello World, from <%= @messenger %>!</h2>
+    """
+  end
 end
